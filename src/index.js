00f6320c172d12d43a1208aa8cb4bf8e4653a36f
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-05-16 22:26:03 -0400

    Add getData function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './index.css';
+
+let data = [];
+
+window.document.getElementById("refresh").addEventListener('click', () => {
+  getScore();
+  console.log("click refresh");
+});
+
+const getScore = () => {
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Pj6OOuAoNFLnlMr5reNl/scores')
+    .then((response) => response.json())
+    .then((json) => {
+      data = json.result;
+      console.log(data)
+    });
+
+}
